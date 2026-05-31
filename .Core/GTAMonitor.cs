6725abe696087966519823f1fb35c900ed43b715
Li Shiyu <li.xiwu@qq.com>
2026-05-31 16:11:17 +0900

    Update GTAMonitor.cs

--- .Core/GTAMonitor.cs
+++ .Core/GTAMonitor.cs
@@ -1,5 +1,6 @@
 using Microsoft.Diagnostics.Tracing;
 using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
 using Microsoft.Diagnostics.Tracing.Session;
 using System.Diagnostics;
 
